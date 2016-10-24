682ac7c58d2eb09fa8dd189774ab154513a4e66a
Elwin Dela Cruz <somi-elwin@somi-elwins-MacBook-Pro.local>
2016-10-24 20:47:41 +0800

    starting files for ptt website

---
+++ web/index.php
@@ -0,0 +1,376 @@
+﻿<!doctype html>
+<html class="no-js" lang="">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>PTT Philippines Corporation | Home</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<meta name="msapplication-TileColor" content="#00adef">
+<meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#00adef">
+
+<link rel="stylesheet" href="css/owl.carousel.css" media="all"/>
+<link rel="stylesheet" href="css/flexslider.css" media="all"/>
+<link rel="stylesheet" href="css/style.css" media="all"/>
+
+<!--[if lt IE 9]> <script src="js/css3-mediaqueries.js"></script> <![endif]-->
+<script src="js/html5.js"></script>
+<script src="js/lib/modernizr-2.8.3.min.js"></script>
+<script src="js/pace.min.js"></script>
+</head>
+<body>
+<section id="main-container">
+	<header>
+		<div class="container">
+			<div class="header-wrapper">
+				<div class="header-hamburger"><span></span></div>
+				<div class="logo"><a href="#"><img src="images/page_template/logo.png" alt=""></a></div>
+				<div class="fr text-right">
+					<div class="helper-wrap">
+						<a href="#" class="helper-btn"><span><i class="fa fa-phone" aria-hidden="true"></i></span>Customer Service</a>
+						<a href="#" class="helper-btn"><span><i class="fa fa-map-marker" aria-hidden="true"></i></span>Station Locator</a>
+						<div class="search-field">
+							<form method="post" action="">
+								<div class="input-wrap">
+									<input type="text" name="" value="" placeholder="Search"></input>
+									<button><i class="fa fa-search" aria-hidden="true"></i></button>
+								</div>	
+							</form>
+						</div>
+					</div>
+					<div class="menu">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Products &amp; Services</a></li>
+							<li><a href="#">Business Opportunity</a></li>
+							<li><a href="#">Promos</a></li>
+							<li><a href="#">Social Responsibility</a></li>
+							<li><a href="#">Press Room</a></li>
+							<li><a href="#">Careers</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="clr"></div>
+			</div>
+
+		</div>
+	</header>
+	<section id="main-wrapper">
+		<div class="banner-wrap">
+			<div class="banner-img animated hiding" data-animation="fadeIn"><img src="images/home/banner.jpg"  width="1800" height="913" alt=""></div>
+			<div class="table-wrap">
+				<div class="table-cell">
+					<div class="container">
+						<h1 class="animated hiding" data-animation="fadeInUp">Add Your Unique Value <br>Proposition Here</h1>
+						<a class="btn animated hiding" data-animation="fadeInUp" href="">Learn More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<section class="feature-wrap bg-blue">
+			<div class="container">
+				<div class="feature-news animated hiding" data-animation="fadeInUp">
+					<div class="flexslider">
+						<ul class="slides">
+							<li>
+								<div class="news-cont-wrap">
+									<div class="nw-img"><a href="#"><img src="images/home/news1.jpg" alt=""></a></div>
+									<div class="nw-text par">
+										<h2>Very Important News Here</h2>
+										<p>Maecenas congue vestibulum nulla, vitae fringilla turpis tempor at. Nunc maximus odio ac diam consequat lobortis. Maecenas non nisl quis lectus egestas rutrum. Donec a diam mauris. Nulla placerat eleifend eros, et gravida mi interdum quis. Aliquam ut ipsum eu risus laoreet blandit efficitur non orci. </p>
+										<a href="#" class="cta-action"> Read the News  <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+									</div>
+									<div class="clr"></div>
+								</div>
+							</li>
+							<li>
+								<div class="news-cont-wrap">
+									<div class="nw-img"><a href="#"><img src="images/home/news1.jpg" alt=""></a></div>
+									<div class="nw-text par">
+										<h2>Main news headline here</h2>
+										<p>Maecenas congue vestibulum nulla, vitae fringilla turpis tempor at. Nunc maximus odio ac diam consequat lobortis. Maecenas non nisl quis lectus egestas rutrum. Donec a diam mauris. Nulla placerat eleifend eros, et gravida mi interdum </p>
+										<a href="#" class="cta-action"> Read the News  <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+									</div>
+									<div class="clr"></div>
+								</div>
+							</li>
+							<li>
+								<div class="news-cont-wrap">
+									<div class="nw-img"><a href="#"><img src="images/home/news1.jpg" alt=""></a></div>
+									<div class="nw-text par">
+										<h2>Main news headline here</h2>
+										<p>Maecenas congue vestibulum nulla, vitae fringilla turpis tempor at. Nunc maximus odio ac diam consequat lobortis. Maecenas non nisl quis </p>
+										<a href="#" class="cta-action"> Read the News  <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+									</div>
+									<div class="clr"></div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="other-feature">
+					<div class="news-cont-wrap">
+						<div class="nw-text show-mobile par">
+							<h2>Very Important News</h2>
+						</div>
+						<div class="nw-img"><a href="#"><img src="images/home/news1.jpg" alt=""></a></div>
+						<div class="nw-text par">
+							<h2 class="hide-in-mobile">Very Important News</h2>
+							<p>Maecenas congue vestibulum nulla, vitae fringilla turpis tempor at. Nunc maximus odio ac diam consequat lobortis. Maecenas non nisl quis lectus egestas rutrum. Donec a diam mauris. Nulla placerat eleifend eros, et gravida mi interdum quis. Aliquam ut ipsum eu risus laoreet blandit efficitur non orci. </p>
+							<a href="#" class="cta-action"> Read the News  <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+						</div>
+						<div class="clr"></div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 animated hiding" data-animation="fadeInUp">
+							<div class="other-news">
+								<h2>Other News</h2>
+								<ul>
+									<li><a href="#">Other news title here</a></li>
+									<li><a href="#">More other news title here</a></li>
+									<li><a href="#">Your news title when it reaches two lines of text</a></li>
+								</ul>
+								<div class="text-center">
+									<a href="#" class="btn btn-transparent">Veiw All News</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-6 animated hiding" data-animation="fadeInUp">
+							<img src="images/home/twitter.png" alt="" style="max-width:100%;">
+						</div>
+						<div class="col-md-4 col-sm-6 fr animated hiding" data-animation="fadeInUp">
+							<div class="subscribe-letter">
+								<h2>Subscribe To Get The Latest News <br> &amp; Updates</h2>
+								<form class="text-center" method="post" action="">
+									<div class="input-wrap">
+										<input type="email" name="" value="" placeholder="Enter Email Here"></input>
+										<input type="submit" class="btn" name="" value="Subscribe"></input>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>	
+		</section>
+
+		<section class="jq-section">
+			<div class="full-boxes">
+				<div class="floating-img fl animated hiding" data-animation="fadeInLeft"><img src="images/home/img-social.jpg" alt=""></div>
+
+				<div class="container">
+					<div class="pos-ab right floating-text">
+						<h3 class="sec-title">Social Responsibility</h3>
+						<p>Maecenas congue vestibulum nulla, vitae fringilla turpis tempor at. Nunc maximus odio ac diam consequat lobortis. Maecenas non nisl quis lectus egestas rutrum. Donec a diam mauris. Nulla placerat eleifend eros, et gravida mi interdum quis. Aliquam ut ipsum eu risus laoreet blandit efficitur non orci. </p>
+						<a href="#" class="btn btn-light">Read more</a>
+					</div>
+				</div>
+				<div class="clr"></div>
+			</div>
+		</section>
+		<section  class="jq-section">
+			<div class="full-boxes">
+				<div class="floating-img fr animated hiding" data-animation="fadeInRight"><img src="images/home/img-partners.jpg" alt=""></div>
+
+				<div class="container">
+					<div class="pos-ab left floating-text">
+						<div class="fl-img"></div>
+						<h3 class="sec-title small">Be Our <br>Partner</h3>
+						<p>Maecenas congue vestibulum nulla, vitae fringilla turpis tempor at. Nunc maximus odio ac diam consequat lobortis. Maecenas non nisl quis lectus egestas rutrum. Donec a diam mauris. Nulla placerat eleifend eros, et gravida mi interdum quis. Aliquam ut ipsum eu risus laoreet blandit efficitur non orci. </p>
+						<a href="#" class="btn btn-light">Read more</a>
+					</div>
+				</div>
+				<div class="clr"></div>
+			</div>
+		</section>
+
+		<section class="promo-wrap sec-pad">
+			<div class="container">
+				<div class="text-center">
+					<h2 class="sec-title animated hiding" data-animation="fadeInUp"> Our Promos</h2>
+				</div>
+				<ul class="promo-list">
+					<li class="animated hiding" data-animation="fadeInUp">
+						<div class="promo-wrap-item">
+							<div class="promo-img"><a href="#"><img src="images/home/promo3.jpg" alt=""></a></div>
+							<div class="promo-text text-center">
+								<h3>Oh My Godji!</h3>
+								<p>Aliquam ultrices mi a mi consectetur blandit. Praesent dictum, risus ut egestas blandit, </p>
+							</div>
+						</div>
+					</li>
+					<li class="animated hiding" data-animation="fadeInUp">
+						<div class="promo-wrap-item">
+							<div class="promo-img"><a href="#"><img src="images/home/promo1.jpg" alt=""></a></div>
+							<div class="promo-text text-center">
+								<h3>PTT + ZAP</h3>
+								<p>Aliquam ultrices mi a mi consectetur blandit. Praesent dictum, risus ut egestas blandit, </p>
+							</div>
+						</div>
+					</li>
+					<li class="animated hiding" data-animation="fadeInUp">
+						<div class="promo-wrap-item">
+							<div class="promo-img"><a href="#"><img src="images/home/promo2.jpg" alt=""></a></div>
+							<div class="promo-text text-center">
+								<h3>PUV DISCOUNT</h3>
+								<p>Aliquam ultrices mi a mi consectetur blandit. Praesent dictum, risus ut egestas blandit, </p>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<div class="text-center animated hiding" data-animation="fadeInUp">
+					<a href="#" class="btn ">View All Promo</a>
+				</div>
+			</div>
+		</section>
+
+		<section class="services-wrap sec-pad">
+			<div class="container">
+				<div class="text-center">
+					<h2 class="sec-title animated hiding" data-animation="fadeInUp">Top Services Here</h2>
+					<ul class="services-list">
+						<li class="animated hiding" data-animation="fadeInUp">
+							<div class="services-wrap-item">
+								<div class="sw-icon"><a href="#"><img src="images/home/icon-services1.png" alt=""></a></div>
+								<div class="sw-text">
+									<h3>service 1 </h3>
+									<p>Maecenas congue vestibulum nulla, vitae fringilla turpis tempor at. Nunc maximus odio ac diam consequat lobortis.</p>
+									<a href="#" class="btn btn-transparent small">Learn More</a>
+								</div>
+							</div>
+						</li>
+						<li class="animated hiding" data-animation="fadeInUp">
+							<div class="services-wrap-item">
+								<div class="sw-icon"><a href="#"><img src="images/home/icon-services2.png" alt=""></a></div>
+								<div class="sw-text">
+									<h3>service 1 </h3>
+									<p>Maecenas congue vestibulum nulla, vitae fringilla turpis tempor at. Nunc maximus odio ac diam consequat lobortis.</p>
+									<a href="#" class="btn btn-transparent small">Learn More</a>
+								</div>
+							</div>
+						</li>
+						<li class="animated hiding" data-animation="fadeInUp">
+							<div class="services-wrap-item">
+								<div class="sw-icon"><a href="#"><img src="images/home/icon-services3.png" alt=""></a></div>
+								<div class="sw-text">
+									<h3>service 1 </h3>
+									<p>Maecenas congue vestibulum nulla, vitae fringilla turpis tempor at. Nunc maximus odio ac diam consequat lobortis.</p>
+									<a href="#" class="btn btn-transparent small">Learn More</a>
+								</div>
+							</div>
+						</li>
+					</ul>
+					<div class="text-center animated hiding" data-animation="fadeInUp">
+						<a href="#" class="btn btn-light">View All</a>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="partners-wrap bg-blue sec-pad">
+			<div class="container">
+				<div class="text-center animated hiding" data-animation="fadeInUp">
+					<h2 class="sec-title st-white">Partners Title Goes Here</h2>
+				</div>
+				<ul class="partners-list">
+					<li class="animated hiding" data-animation="fadeInUp" data-delay="100"><img src="images/home/partners6.png" alt=""></li>
+					<li class="animated hiding" data-animation="fadeInUp" data-delay="150"><img src="images/home/partners5.png" alt=""></li>
+					<li class="animated hiding" data-animation="fadeInUp" data-delay="200"><img src="images/home/partners4.png" alt=""></li>
+					<li class="animated hiding" data-animation="fadeInUp" data-delay="250"><img src="images/home/partners3.png" alt=""></li>
+					<li class="animated hiding" data-animation="fadeInUp" data-delay="300"><img src="images/home/partners2.png" alt=""></li>
+					<li class="animated hiding" data-animation="fadeInUp" data-delay="350"><img src="images/home/partners1.png" alt=""></li>
+				</ul>
+			</div>
+		</section>
+	</section>
+</section>
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="footer-desc par">
+					<div class="f-logo">
+						<img src="images/page_template/logo.png" alt="">
+					</div>
+					<h5>ADDRESS</h5>
+					<p>32/F LKG Tower, 6801 Ayala Avenue <br>Makati City 1226 Philippines</p>
+					<h5>TELEPHONE</h5>
+					<p><a href="tel:+1 800 603 6035">+1 800 603 6035</a></p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="footer-menu">
+					<h4 class="f-title">CUSTOMER SERVICES</h4>
+					<ul>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Terms and Conditions</a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<h4 class="f-title">Instagram Photos</h4>
+				<ul class="instag-list">
+					<li>
+						<div class="instag-img"><a href="#"><img src="images/home/instag2.jpg" alt=""></a></div>
+					</li>
+					<li>
+						<div class="instag-img"><a href="#"><img src="images/home/instag3.jpg" alt=""></a></div>
+					</li>
+					<li>
+						<div class="instag-img"><a href="#"><img src="images/home/instag1.jpg" alt=""></a></div>
+					</li>
+					<li>
+						<div class="instag-img"><a href="#"><img src="images/home/instag7.jpg" alt=""></a></div>
+					</li>
+					<li>
+						<div class="instag-img"><a href="#"><img src="images/home/instag6.jpg" alt=""></a></div>
+					</li>
+					<li>
+						<div class="instag-img"><a href="#"><img src="images/home/instag8.jpg" alt=""></a></div>
+					</li>
+					
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<div class="subscription-wrap">
+					<h4 class="f-title">Subscribe</h4>
+					<p>Subscribe to our newsletter for more information </p>
+					<form method="post" action="">
+						<div class="input-wrap">
+							<input type="email" name="" value="" placeholder="Enter your email address"></input>
+							<input type="submit" name="" value="Submit"></input>
+						</div>	
+					</form>
+					<h4 class="f-title">Follow Us</h4>
+					<ul class="social-media">
+						<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+						<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>	
+			</div>
+
+		</div>
+	</div>
+	<div class="copyright-wrap">
+		<div class="container">
+			<p>Copyright &copy; 2016 PTT Philippines Corporation. All rights reserved.</p>
+		</div>
+	</div>
+</footer>
+<script src="js/lib/jquery.min.js"></script>
+<script src="js/lib/jquery-ui.min.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/jquery.appear.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
---
+++ web/js/custom.js
@@ -0,0 +1,189 @@
+//get all elements with class and get the biggest box
+function get_biggest(elements){
+	var biggest_height = 0;
+	for ( var i = 0; i < elements.length ; i++ ){
+		var element_height = $(elements[i]).height();
+		//compare the height, if bigger, assign to variable
+		if(element_height > biggest_height ) biggest_height = element_height;
+	}
+	return biggest_height;
+}
+
+function resize() {
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+
+	// STICKY FOOTER
+	var headerHeight = $('header').outerHeight();
+	var footerHeight = $('footer').outerHeight();
+	var footerTop = (footerHeight) * -1
+	$('footer').css({marginTop: footerTop});
+	$('#main-wrapper').css({paddingBottom: footerHeight});
+
+	$('.banner-wrap').css({'height' : windowHeight + 100});
+
+	// for vertically middle content
+	$('.bp-middle').each(function() {
+		var bpMiddleHeight = $(this).outerHeight() / 2 * - 1;
+		$(this).css({marginTop: bpMiddleHeight});
+	});
+
+	// for equalizer
+	$('.classname').css({minHeight: 0});
+  var ClassName = get_biggest($('.classname'));
+  $('.classname').css({minHeight: ClassName});
+}
+
+$(window).resize(function() {
+	resize();
+	doCoverImage();
+
+	if ($(window).width() < 992) {
+		$('header').addClass('header-white');
+	} else {
+		$('header').removeClass('header-white');
+	}
+});
+
+$(document).ready(function() {
+	doCoverImage();
+	resize();
+
+	if (Modernizr.touch) {
+		$('html').addClass('bp-touch');
+	}
+
+	if ($(window).width() < 992)  {
+		$('header').addClass('header-white');
+	} else {
+		$('header').removeClass('header-white');
+	}
+
+	$('.header-hamburger').click(function(){
+    	var _this = $(this);
+
+        setTimeout(function(){
+            _this.toggleClass('active');
+        }, 300);
+          
+    });
+
+    if($(window).width() < 992) {
+		$('.hiding').css({'opacity' : 1});
+	} else {
+		$('.animated').appear(function() {
+	        var element = $(this);
+	        var animation = element.data('animation');
+	        var animationDelay = element.data('delay');
+	        if(animationDelay) {
+	          setTimeout(function(){
+	              element.addClass( animation + " visible" );
+	              element.removeClass('hiding');
+	          }, animationDelay);
+	        } else {
+	          element.addClass( animation + " visible" );
+	          element.removeClass('hiding');
+	        }               
+	    }, {accY: -90});
+	}
+
+	
+});
+
+$(window).load(function() {
+	resize();
+	doCoverImage();
+
+	if ($(window).width() < 991) {
+		$('header').addClass('header-white');
+	} else {
+		$('header').removeClass('header-white');
+	}
+
+	setTimeout(function(){
+		$('header').addClass('is-animated');
+	}, 300);
+
+	$('.feature-news .flexslider').flexslider({
+	    animation: "fade",
+	    controlNav: true,
+	    directionNav: false,
+	    smoothHeight: true
+
+	});
+});
+
+// preloader once done
+Pace.on('done', function() {
+	// totally hide the preloader especially for IE
+	setTimeout(function() {
+		$('.pace-inactive').hide();
+	}, 500);
+});
+
+$(window).on('scroll load', function(){
+
+    var _cur_top = $(window).scrollTop();
+	if(  _cur_top >=  50) {
+		$('header').addClass('is-fixed');
+
+	} else {
+		$('header').removeClass('is-fixed');
+	}
+
+});
+
+
+function doCoverImage() {	
+	$('.banner-img img').each(function() {
+		coverImage( $(this) );
+	});
+	
+}
+
+function coverImage( image ) {
+
+	var imgObj = image;
+	var iW = imgObj.attr('width');//width(); //width of image ratio
+	var iH = imgObj.attr('height');//.height(); //height of image ratio
+
+	imgObj.width(0).height(0);
+	
+	var imgContainer = image.parent();
+	var cW = imgContainer.width(); //width of container or browser
+	var cH = imgContainer.height(); //height of container or browser
+
+	//console.log(iW,iH,cW,cH);
+
+	if ( cH > 1 ) {
+		var cP = cW/cH; //ratio of container or browser
+		var iP = iW/iH; //ratio of image
+
+		if ( iP > cP ) { //if image ratio is more than container ratio (if image width is more than container width)
+			iH = cH; //set image height from container height
+			iW = cH * iP; //set image width using container height and image ratio
+			imgObj.css({
+				'margin-top': 0,
+				'margin-left': Math.ceil((cW-iW)/2),
+				'width': Math.ceil(iW),
+				'height': Math.ceil(iH)
+			}); //center the image and set dimensions
+		} else { //if image ratio is less than container ratio (if image height is more than container height)
+			iW = cW; //set image width from container width
+			iH = cW / iP; //set image height from container width and ratio
+			imgObj.css({
+				'margin-top': Math.ceil((cH-iH)/2),
+				'margin-left': 0,
+				'width': Math.ceil(iW),
+				'height': Math.ceil(iH)
+			}); //center the image and set dimensions
+		}
+	} else {
+		imgObj.css({
+			'margin-top': 0,
+			'margin-left': 0,
+			'width': 'auto',
+			'height': 'auto'
+		});
+	}
+}
